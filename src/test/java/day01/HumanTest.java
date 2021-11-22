4e502f138141e38c1d96f5c7a3976888bb0b8866
Szalay Tamás <szalaytamas90@gmail.com>
2021-11-22 15:48:01 +0100

    human,test

--- src/test/java/day01/HumanTest.java
+++ src/test/java/day01/HumanTest.java
@@ -5,10 +5,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HumanTest {
+
+    private Human human;
     @Test
     void aWorkingTest(){
         Human human = new Human("Józska Pista", 100);
         assertEquals("Józska Pista",human.getName());
         assertEquals(100 , human.getAge());
+    }
+    @Test
+    void aTestForThrowAge(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                human = new Human("Józksa", 1));
+        assertEquals("a név nem két részből áll", exception.getMessage());
+    }
+    @Test
+    void aTestForThrowName(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                human = new Human("Józskaa Pista" , 121));
+        assertEquals("senki se lehet 120évesnél öregebb", exception.getMessage());
     }
 }
